5dd134f6021a8a30c505f88e0b5350e25373d98e
LucasDvol <lucasdvol@gmail.com>
2024-12-03 22:45:53 -0300

    Update power_bi_insights_tab.py

--- tabs/analise/power_bi_insights_tab.py
+++ tabs/analise/power_bi_insights_tab.py
@@ -66,38 +66,4 @@ class PowerBIInsightsTab(TabInterface):
 
                 st.image("assets/img/Eventos_05.png", caption="Análise dos Eventos-Chave 2011-2014 que Impactaram os Preços do Petróleo Brent (Power BI)")
                 
-                      st.markdown(
-                """
-                :six: **Descobertas e Explorações:**\n
-                Descobertas de novos campos petrolíferos e a exploração em áreas anteriormente inacessíveis, como o Ártico, podem aumentar a oferta de petróleo. Tais descobertas podem estabilizar ou até reduzir os preços, dependendo da magnitude das reservas encontradas e da viabilidade econômica da exploração. A exploração no Ártico e no offshore profundo são exemplos de áreas que estão sendo cada vez mais investigadas.\n
-                :seven: **Crise da Dívida Europeia:**\n
-                A crise da dívida europeia, iniciada em 2010, teve um impacto significativo no mercado de petróleo. A incerteza econômica e as medidas de austeridade implementadas em vários países europeus levaram a uma diminuição da demanda por petróleo na região, contribuindo para a volatilidade dos preços. Esse período destacou a interconectividade das economias globais e seus efeitos sobre os mercados de commodities.
-                """
-            )
-
-            st.image("assets/img/Eventos_03_PBI.jpeg", caption="Queda significativa no mercado de petróleo com a crise da dívida europeia (Power BI)")
-
-            st.markdown(
-                """    
-                :eight: **Colapso do Lehman Brothers:**\n
-                O colapso do Lehman Brothers em 2008 marcou o início de uma profunda crise financeira global. Esse evento levou a uma retração severa na economia mundial, resultando em uma queda acentuada na demanda por petróleo e uma subsequente queda nos preços. O impacto desse colapso demonstrou como eventos financeiros podem rapidamente se espalhar e afetar mercados aparentemente não relacionados, como o de petróleo.
-                """
-            )
-
-            st.image("assets/img/Eventos_04_PBI.jpeg", caption="Profunda crise financeira global com o colapso do Lehman Brothers em 2008 (Power BI)")
-
-            st.subheader(":orange[Análises de Especialistas]")
-
-            st.markdown(
-                """   
-                Analistas do mercado de petróleo frequentemente monitoram uma combinação de fatores para fazer previsões sobre os preços. Estes incluem relatórios de produção da OPEP, estoques de petróleo dos EUA, dados de consumo energético global e indicadores econômicos gerais. A integração de análises quantitativas e qualitativas é crucial para formar uma visão holística dos possíveis movimentos de preços.
-                """
-            )
-
-            st.subheader(":orange[Importância da Análise de Eventos]")
-
-            st.markdown(
-                """  
-                Identificar e entender esses eventos é crucial para prever movimentos futuros nos preços do petróleo. Investidores, governos e empresas de energia dependem dessas análises para tomar decisões informadas, gerenciar riscos e formular estratégias de longo prazo. O monitoramento contínuo e a análise detalhada dos eventos e seus impactos permitem uma resposta proativa às mudanças do mercado, contribuindo para a estabilidade econômica e a segurança energética.
-                """
-            )
+                

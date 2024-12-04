3d4ed8e2e8da08e661d248bc5de7fb274a9b8ae2
LucasDvol <lucasdvol@gmail.com>
2024-12-03 22:29:17 -0300

    Update power_bi_insights_tab.py

--- tabs/analise/power_bi_insights_tab.py
+++ tabs/analise/power_bi_insights_tab.py
@@ -23,9 +23,7 @@ class PowerBIInsightsTab(TabInterface):
                 """
                 A compreensão dos eventos que impactam o mercado de petróleo é essencial para prever flutuações nos preços. Eventos específicos podem ter impactos significativos e variados nos preços do petróleo, influenciando diretamente a oferta e a demanda.\n
                 :one: **2011-2014 (Preços elevados, acima de 100 USD):**\n
-                A alta foi impulsionada pela instabilidade política no Oriente Médio e Norte da África, decorrente da Primavera Árabe (2011).
-Sanções econômicas contra o Irã restringiram a oferta de petróleo no mercado global.
-O crescimento econômico global pós-crise de 2008 também sustentou uma forte demanda.\n
+                Interrupções na oferta de petróleo, como conflitos geopolíticos, desastres naturais ou sanções econômicas, frequentemente resultam em aumentos nos preços. Por exemplo, a guerra no Oriente Médio ou sanções impostas a países exportadores de petróleo podem restringir a oferta, levando a um aumento nos preços globais. A crise do petróleo de 1973, causada pelo embargo da OPEP, levou a um aumento drástico dos preços e teve consequências econômicas profundas em nível global.\n
                 """
             )
 

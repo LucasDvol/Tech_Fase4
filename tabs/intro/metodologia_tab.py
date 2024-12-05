d67a264c1c64569b96adc685c24fffe14d85a0e2
LucasDvol <lucasdvol@gmail.com>
2024-12-05 08:07:10 -0300

    Update metodologia_tab.py

--- tabs/intro/metodologia_tab.py
+++ tabs/intro/metodologia_tab.py
@@ -14,5 +14,5 @@ class MetodologiaTab(TabInterface):
                 Para a realização deste trabalho, utilizou-se a ferramenta Power BI para a criação de gráficos interativos, aliada ao Streamlit para o desenvolvimento do MVP (Minimum Viable Product, ou Produto Mínimo Viável) do modelo em produção. Essa combinação permitiu a criação de um dashboard dinâmico e informativo, essencial para fornecer insights sobre o preço do petróleo e suas influências.
             ''')
 
-            st.image('assets/img/streamlit_powerbi.png', caption='MVP: Streamlit + PowerBI')
+            #st.image('assets/img/streamlit_powerbi.png', caption='MVP: Streamlit + PowerBI')
             
